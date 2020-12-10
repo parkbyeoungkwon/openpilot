368766e76028f22c57e1819e3c36367055151a3b
parkbyeoungkwon <72653549+parkbyeoungkwon@users.noreply.github.com>
2020-12-10 20:06:16 +0900

    가감속부분 수정

--- selfdrive/car/hyundai/scc_smoother.py
+++ selfdrive/car/hyundai/scc_smoother.py
@@ -262,19 +262,43 @@ class SccSmoother:
 
       d = lead.dRel - 5.
 
-      if 0. < d < -lead.vRel * (9. + cruise_gap) * 2. and lead.vRel < -1.:
-        t = d / lead.vRel
+      
+#munseongsik github start   
+      if 0. < d < -lead.vRel * (7.6872 + cruise_gap) * 2. and lead.vRel < -1.:
+        t = d / lead.vRel * 0.98
         acc = -(lead.vRel / t) * CV.MS_TO_KPH * 1.8
         override_acc = acc
         accel = (op_accel + acc) / 2.
-      else:
-        accel = op_accel
-
+      else:        
+        accel = op_accel * interp(clu11_speed, [0., 20., 35., 50., 51., 60., 100.], [2.3, 3.28, 3.0, 1.7, 1.65, 1.4, 1.0])
+#        if 35 > lead.dRel > 15:
+#          if clu11_speed < 50:
+#            accel = op_accel * 2.5               
+#        else:
+#          accel = op_accel * interp(clu11_speed, [50., 60., 100.], [1.75, 1.4, 1.0])
     if accel > 0.:
-      accel *= self.accel_gain * interp(clu11_speed, [30., 100.], [1.5, 1.2])
+      accel *= self.accel_gain * interp(clu11_speed, [35., 60., 100.], [1.5, 1.25, 1.2])
     else:
-      accel *= self.decel_gain * 1.8
+      accel *= self.decel_gain * interp(clu11_speed, [70., 75.], [1.79285, 1.8])      
+#munseongsik github end      
+      
+            
+#original start     
+      #if 0. < d < -lead.vRel * (9. + cruise_gap) * 2. and lead.vRel < -1.:
+        #t = d / lead.vRel
+        #acc = -(lead.vRel / t) * CV.MS_TO_KPH * 1.8
+        #override_acc = acc
+        #accel = (op_accel + acc) / 2.
+      #else:
+        #accel = op_accel
 
+    #if accel > 0.:
+      #accel *= self.accel_gain * interp(clu11_speed, [30., 100.], [1.5, 1.2])
+    #else:
+      #accel *= self.decel_gain * 1.8
+#original end
+      
+      
     return clip(accel, -LIMIT_DECEL, LIMIT_ACCEL), override_acc
 
   def get_curve_speed(self, sm, v_ego):
